533267e1aa1964224f8614a741fd304193af0b8b
HieuTong <hieuminhtongle20111998@gmail.com>
2022-05-09 00:53:28 +0700

    Set up tabbbar

---
+++ LearningNetflix/Controllers/DownloadsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DownloadsViewController.swift
+//  LearningNetflix
+//
+//  Created by HieuTong on 09/05/2022.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
