4b43c646f6d15fe30d7f660ff38244ca137d304e
HieuTong <hieuminhtongle20111998@gmail.com>
2022-05-09 08:33:30 +0700

    Set up homeViewController

---
+++ LearningNetflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  CollectionViewTableViewCell.swift
+//  LearningNetflix
+//
+//  Created by HieuTong on 09/05/2022.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionView.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
