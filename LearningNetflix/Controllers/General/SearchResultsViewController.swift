38dfb310f8d7a150707861992baf9d03cf6e3387
HieuLe201198 <hieu.le@coinhako.com>
2022-06-16 21:33:47 +0700

    Search viewcontroller

--- LearningNetflix/Controllers/General/SearchResultsViewController.swift
+++ LearningNetflix/Controllers/General/SearchResultsViewController.swift
@@ -8,9 +8,42 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    private var titles = [Title]()
+    
+    private let searchResultsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 200)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleTableViewCell.self, forCellWithReuseIdentifier: TitleTableViewCell.identifier)
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
+        
+        searchResultsCollectionView.delegate = self
+        searchResultsCollectionView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchResultsCollectionView.frame = view.bounds
+    }
+}
 
+extension SearchResultsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
 }
